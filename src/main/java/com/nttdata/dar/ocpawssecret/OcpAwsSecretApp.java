003074b666afbb9bfa518a2796180b2748ba5b79
Vanessa Ruiz Robledinos <vruizrob@emeal.nttdata.com>
2022-10-13 14:24:46 +0200

    aaaa

--- src/main/java/com/nttdata/dar/ocpawssecret/OcpAwsSecretApp.java
+++ src/main/java/com/nttdata/dar/ocpawssecret/OcpAwsSecretApp.java
@@ -1,18 +1,35 @@
 package com.nttdata.dar.ocpawssecret;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @ComponentScan
 @SpringBootApplication
-public class OcpAwsSecretApp {
+public class OcpAwsSecretApp extends WebSecurityConfigurerAdapter {
 
-   
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception 
+    {
+        auth.inMemoryAuthentication()
+            .withUser("ocpaws")
+            .password("{noop}secret")
+            .roles("USER");
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(OcpAwsSecretApp.class, args);
     }
 }
-
 
